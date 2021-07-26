d2005224d904ebc9274b60db05ef3348c168a1f6
Sai Krishna <saikrishna@Administrators-MacBook-Pro.local>
2021-07-26 17:34:12 +0530

    Resolved Conflicts

--- src/test/java/com/itt/oms/tests/BuyerAllMenuPageBuy8PreProdTests.java
+++ src/test/java/com/itt/oms/tests/BuyerAllMenuPageBuy8PreProdTests.java
@@ -7,13 +7,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.itt.basetest.OMSBaseTest;
+import com.itt.basetest.ITTBaseTest;
 import com.itt.oms.datamodelhelper.OMSDataModelHelperFactory;
 import com.itt.oms.pages.login.OMSLoginPage.USER;
 import com.itt.parser.testdata.TestData;
 import com.itt.parser.testdata.TestDataProviders;
 
-public class BuyerAllMenuPageBuy8PreProdTests extends OMSBaseTest {
+public class BuyerAllMenuPageBuy8PreProdTests extends ITTBaseTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BuyerAllMenuPageBuy8PreProdTests.class);
     private String tabName;
